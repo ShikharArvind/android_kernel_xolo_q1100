6f1390984f7dd090520c92bacca16c351bbd9683
ShikharArvind <shikharavind@gmail.com>
2015-12-09 12:23:15 +0530

    Add String For Eg

--- merge_fix.sh
+++ merge_fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type f -name -exec sed -e 's/\( \).*\( \)/\1\2/' {} \;  #Dont miss the semicolon
-grep -rl ' ' ./ | xargs sed -i 's/>>>>>>> remotes\/remi\/LNX.LA.3.5.1-10110-8x26.0/ /g'
-grep -rl ' ' ./ | xargs sed -i 's/ / /g'
-grep -rl ' ' ./ | xargs sed -i 's/ / /g' #Careful  
+find . -type f -name -exec sed -e 's/\(<<<<<<< HEAD\).*\(=======\)/\1\2/' {} \;  #Dont miss the semicolon
+grep -rl '<<<<<<< HEAD' ./ | xargs sed -i 's/=======/ /g' #Careful  many files have this phrase
+grep -rl '<<<<<<< HEAD' ./ | xargs sed -i 's/<<<<<<< HEAD/ /g'
+grep -rl ' >>>>>>> remotes\/remi\/LNX.LA.3.5.1-10110-8x26.0' ./ | xargs sed -i 's/>>>>>>> remotes\/remi\/LNX.LA.3.5.1-10110-8x26.0/ /g'
